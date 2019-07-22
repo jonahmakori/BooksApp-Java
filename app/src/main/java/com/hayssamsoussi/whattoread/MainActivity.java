3d66cf12e739f0b40ea1ae22bb7fa617954ebd43
Hayssam Soussi <hayssamsoussi@gmail.com>
2019-07-22 04:20:34 +0300

    - Code optimized

--- app/src/main/java/com/hayssamsoussi/whattoread/MainActivity.java
+++ app/src/main/java/com/hayssamsoussi/whattoread/MainActivity.java
@@ -4,47 +4,23 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import com.hayssamsoussi.whattoread.fragments.BooksFragment;
 import com.hayssamsoussi.whattoread.fragments.CategoriesFragment;
 import com.hayssamsoussi.whattoread.fragments.FavoritesFragment;
-import com.hayssamsoussi.whattoread.models.Example;
-import com.hayssamsoussi.whattoread.models.Item;
 
-import java.util.List;
-
-import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Call<Example> mCallBooks;
-    private List<Item> mItems;
-
-    //layout elements
-    private LinearLayout fragmentContainer;
-    private ImageView mProfilePicture;
-    private TextView mProfileName;
-    private TextView mProfileLbl;
-    private Toolbar toolbar;
-    private Bundle bundle;
-    private Fragment movieFragment, favoritesFragment, categoriesFragment;
-    private FragmentManager fragmentManager;
-    private FragmentTransaction fragmentTransaction;
-
     //NEW
-    final Fragment fragment1 = new BooksFragment();
-    final Fragment fragment2 = new CategoriesFragment();
-    final Fragment fragment3 = new FavoritesFragment();
-    final FragmentManager fm = getSupportFragmentManager();
-    Fragment active = fragment1;
+    private final Fragment fragment1 = new BooksFragment();
+    private final Fragment fragment2 = new CategoriesFragment();
+    private final Fragment fragment3 = new FavoritesFragment();
+    private final FragmentManager fm = getSupportFragmentManager();
+    private Fragment active = fragment1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,11 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
         //Fragments will not be recreated
         fm.beginTransaction().add(R.id.fragment_container, fragment3, "3").hide(fragment3).commit();
-        fm.beginTransaction().add(R.id.fragment_container, fragment3, "3").hide(fragment3).commit();
         fm.beginTransaction().add(R.id.fragment_container, fragment2, "2").hide(fragment2).commit();
         fm.beginTransaction().add(R.id.fragment_container, fragment1, "1").commit();
 
-        fragmentContainer = (LinearLayout) findViewById(R.id.fragment_container);
+        LinearLayout fragmentContainer = (LinearLayout) findViewById(R.id.fragment_container);
         BottomNavigationView mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_main);
 
         // listener for botton navigation menu
@@ -80,8 +55,11 @@ public class MainActivity extends AppCompatActivity {
                         fm.beginTransaction().hide(active).show(fragment3).commit();
                         active = fragment3;
                         return true;
+
+                    default:
+                        return false;
+
                 }
-                return false;
             }
         });
     }
@@ -91,9 +69,4 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-
-
-    private void setupToolbar() {
-        setSupportActionBar(toolbar);
-    }
 }
